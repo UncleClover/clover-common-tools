f85030961d8df5cba9dff24dc6ce2b51918af684
zhangdq <zhangdq@asiainfo.com>
2017-12-13 16:19:24 +0800

    增加港澳台

--- src/main/java/com/clover/tools/common/constants/ProvinceCode.java
+++ src/main/java/com/clover/tools/common/constants/ProvinceCode.java
@@ -10,15 +10,18 @@ import java.util.Random;
  * @Email qiang900714@126.com
  */
 public enum ProvinceCode {
-	bj("11", "北京"), tj("12", "天津"), he("13", "河北"), sx("14", "山西"), nm("15", "内蒙古"), ln("21", "辽宁"), 
-	jl("22", "吉林"), hlj("23", "黑龙江"), sh("31", "上海"), js("32", "江苏"), zj("33", "浙江"), ah("34", "安徽"), 
-	fj("35", "福建"), jx("36", "江西"), sd("37", "山东"), ha("41", "河南"), hb("42", "湖北"), hn("43", "湖南"), 
-	gd("44", "广东"), gx("45", "广西"), hi("46", "海南"), cq("50", "重庆"), sc("51", "四川"), gz("52", "贵州"), 
-	yn("53", "云南"), xz("54", "西藏"), sn("61", "陕西"), gs("62", "甘肃"), qh("63", "青海"), nx("64", "宁夏"), 
-	xj("65", "新疆"), nonentity("", "");
+	bj("11", "北京市"), tj("12", "天津市"), he("13", "河北省"), sx("14", "山西省"), nm("15", "内蒙古自治区"), 
+	ln("21", "辽宁省"), jl("22", "吉林省"), hlj("23", "黑龙江省"), sh("31", "上海市"), js("32", "江苏省"), 
+	zj("33", "浙江省"), ah("34", "安徽省"), fj("35", "福建省"), jx("36", "江西省"), sd("37", "山东省"), 
+	ha("41", "河南省"), hb("42", "湖北省"), hn("43", "湖南省"), gd("44", "广东省"), gx("45", "广西壮族自治区"), 
+	hi("46", "海南省"), cq("50", "重庆市"), sc("51", "四川省"), gz("52", "贵州省"), yn("53", "云南省"), 
+	xz("54", "西藏自治区"), sn("61", "陕西省"), gs("62", "甘肃省"), qh("63", "青海省"), nx("64", "宁夏回族自治区"), 
+	xj("65", "新疆维吾尔自治区"), tw("71", "台湾省"), xg("81", "香港特别行政区"), am("82", "澳门特别行政区"), 
+	nonentity("", "");
 
 	private String name;
 	private String code;
+
 	ProvinceCode(String code, String name) {
 		this.name = name;
 		this.code = code;
@@ -39,7 +42,7 @@ public enum ProvinceCode {
 	public String getName() {
 		return name;
 	}
-	
+
 	/**
 	 * 根据code查询枚举信息
 	 * 
@@ -75,7 +78,7 @@ public enum ProvinceCode {
 		}
 		return ProvinceCode.nonentity;
 	}
-	
+
 	/**
 	 * 获取枚举长度
 	 * 
@@ -87,7 +90,7 @@ public enum ProvinceCode {
 	public static int length() {
 		return ProvinceCode.values().length - 1;
 	}
-	
+
 	/**
 	 * 随机获取其中一个省份枚举信息
 	 * 
@@ -96,7 +99,7 @@ public enum ProvinceCode {
 	 * @Email qiang900714@126.com
 	 * @return
 	 */
-	public static ProvinceCode random(){
+	public static ProvinceCode random() {
 		return ProvinceCode.values()[new Random(System.currentTimeMillis()).nextInt(length())];
 	}
 }
