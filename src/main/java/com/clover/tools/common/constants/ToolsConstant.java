78c395868aff08f0fdfbe1dec04d8282e4a188b6
zhangdq <zhangdq@asiainfo.com>
2017-12-14 18:40:25 +0800

    增加常量累

---
+++ src/main/java/com/clover/tools/common/constants/ToolsConstant.java
@@ -0,0 +1,17 @@
+package com.clover.tools.common.constants;
+
+public class ToolsConstant {
+	private ToolsConstant() {
+	}
+
+	public final static class DateParam {
+		private DateParam() {
+
+		}
+
+		public final static int DEFAULT_YEAR = 100;
+		public final static int YMD = 10000;
+		public final static String PRE_DATE = "pre";
+		public final static String POS_DATE = "pos";
+	}
+}
